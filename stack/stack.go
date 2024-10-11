6f53e6be00c30f36a1b7f637d96c8dfdba523747
Styg <kioravuser@gmail.com>
2024-10-11 16:08:52 +0300

    Add limited stack.

--- stack/stack.go
+++ stack/stack.go
@@ -9,7 +9,7 @@ type Stack[V any] struct {
 var EmptyStackError = fmt.Errorf("Trying to pop an empty stack.")
 
 func NewStack[V any]() Stack[V] {
-	return Stack[V]{}
+	return Stack[V]{s: []V{}}
 }
 
 func (this *Stack[V]) Push(vals ...V) {
@@ -29,4 +29,44 @@ func (this *Stack[V]) Pop() (V, error) {
 
 func (this Stack[V]) Len() int {
 	return len(this.s)
+}
+
+type LimitedStack[V any] struct {
+	s   []V
+	cap int
+}
+
+var StackOverflowError = fmt.Errorf("Trying to push to a full stack.")
+
+func NewLimitedStack[V any](cap int) LimitedStack[V] {
+	return LimitedStack[V]{cap: cap, s: []V{}}
+}
+
+func (this *LimitedStack[V]) Push(vals ...V) error {
+	if len(this.s) + len(vals) > this.cap {
+		return StackOverflowError
+	}
+
+	this.s = append(this.s, vals...)
+
+	return nil
+}
+
+func (this *LimitedStack[V]) Pop() (V, error) {
+	if len(this.s) == 0 {
+		return *new(V), EmptyStackError
+	}
+
+	res := this.s[len(this.s)-1]
+	this.s = this.s[:len(this.s)-1]
+
+	return res, nil
+}
+
+func (this LimitedStack[V]) Len() int {
+	return len(this.s)
+}
+
+func (this LimitedStack[V]) Cap() int {
+	return this.cap
 }
