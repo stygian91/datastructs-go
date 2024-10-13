531e9e0246974b7666b57d2e09d3ef0225c18256
Styg <kioravuser@gmail.com>
2024-10-13 11:34:26 +0300

    BST.Remove() - return actual node value instead of pointer.
    Fixes returning pointer to a removed value.

--- bst/bst.go
+++ bst/bst.go
@@ -123,15 +123,15 @@ func (this *Node[T, M]) Add(value T, meta M) {
 	}
 }
 
-func (this *BST[T, M]) Remove(value T) (*Node[T, M], bool) {
+func (this *BST[T, M]) Remove(value T) (Node[T, M], bool) {
 	values := []NodeValue[T, M]{}
 	found := false
-	foundNode := &Node[T, M]{}
+	foundNode := Node[T, M]{}
 
 	for node := range this.InOrderSeq() {
 		if !found && node.Value == value {
 			found = true
-			foundNode = node
+			foundNode = *node
 			continue
 		}
 
